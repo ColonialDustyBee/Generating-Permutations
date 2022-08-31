eff793d3ddd8c9c648e113e618b6ceacf3966535
Jesus <111938653+ColonialDustyBee@users.noreply.github.com>
2022-08-30 18:50:24 -0700

    Added add position
    Added while loop that loops it from right to left, however, there's still bugs that need to be ironed out.

--- permutations.cpp
+++ permutations.cpp
@@ -31,20 +31,38 @@ void Permutations::generateList(int userInput) {
 
 	list = localList;
 }
-void Permutations::generatePermutations() {
+void Permutations::generatePermutations(int userInput) {
+	int factorialValue = permutationValue;
 	vector <int> localList = list;
 	vector<int>::iterator begin = localList.begin();// Should gather beginning
-	vector<int>::iterator end = localList.end();
+	vector<int>::iterator end = localList.end() - 1;
 	vector <int>::iterator position = localList.end() - 1; // Should gather the position to the very end and then decrease
-	while (*position > *position - 1 && *position != *begin) { // while the mobile element is greater than the one in front of it AND it's not at the beginning
-		if (*position > *position - 1) { // if current one is greater than the one before it (it starts on the right)
-			swap(*position, *(position - 1)); // swap it.
-			for (auto i : localList) { // prints permutation
-				cout << i << " ";
+	while (factorialValue > 0) {
+		while (position != begin) { // while the mobile element is greater than the one in front of it AND it's not at the beginning
+			if (*position > *position - 1) { // if current one is greater than the one before it (it starts on the right)
+				swap(*position, *(position - 1)); // swap it.
+				for (auto i : localList) { // prints permutation
+					cout << i << " ";
+				}
+				cout << endl;
+				position--; // decrements
+				cout << factorialValue << endl;
+				factorialValue--;
 			}
-			cout << endl;
 		}
-		--position; // decrements.
+		
+		while (position != end) {
+			if (*position < *position + 1) {
+				swap(*(position), *(position + 1));
+				for (auto i : localList) {
+					cout << i << " ";
+				}
+				cout << endl;
+				position++;
+				cout << factorialValue << endl;
+				factorialValue--;
+			}
+		}
 	}
 }
 
