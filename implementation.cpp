4e67f22ace8e0a859f582c6dbeee0defaf54e4ac
Jesus <111938653+ColonialDustyBee@users.noreply.github.com>
2022-08-31 23:23:13 -0700

    Updated test driver code
    Made input loop that calls new validation method.

--- implementation.cpp
+++ implementation.cpp
@@ -6,6 +6,23 @@ using namespace std;
 
 
 int main() {
-	
+	Permutations permutations;
+	int input = 0;
+	bool incorrectInput = false;
+	cout << "Please enter a value from 1 to 25" << endl;
+	cin >> input;
+	while (!incorrectInput) {
+		incorrectInput = permutations.userValidation(input);
+		if (!incorrectInput) {
+			cout << "Value is incorrect!" << endl;
+			cout << "Please enter again: ";
+			cin >> input;
+			incorrectInput = permutations.userValidation(input);
+		}
+	}
+	permutations.findFactorial(input);
+	cout << "The factorial of: " << input << "is: " << permutations.getFactorial() << endl;
+	permutations.generateList(input);
+	permutations.generatePermutations(input);
 	return 0;
 }
